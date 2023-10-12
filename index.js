db876733764c403a4f64bbc208911d9d3331099d
Jana Salah <janaabuhaltam@gmail.com>
2023-10-12 15:24:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordArray = Array.from(word)
+  console.log(wordArray)
+  const backwardsArray = wordArray.reverse()
+  console.log(backwardsArray)
+
+ for (let i=0; i<wordArray.length;i++){
+    let a = wordArray[i]
+    let b = backwardsArray[(wordArray.length-1) - i]
+    if(a !== b){
+      return false
+    }
+  }
+
+  return true
 }
 
 /* 
   Add your pseudocode here
+  1- convert string to an array
+  2- reverse the array values
+  3- loop and compare each item in the array
+  4- return true if it is equal 
+  5- Otherwise, return false
 */
 
 /*
